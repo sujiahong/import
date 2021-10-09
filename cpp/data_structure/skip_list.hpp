810c1d99ccca502ae74e1c758bf18b9b8c861ca3
sujiahong <sujiahong>
2021-10-09 22:20:41 +0800

    跳表修改

--- cpp/data_structure/skip_list.hpp
+++ cpp/data_structure/skip_list.hpp
@@ -1,14 +1,18 @@
 #ifndef _SKIP_LIST_HPP_
 #define _SKIP_LIST_HPP_
 
+#include "../algorithm/ramdom_disorder.hpp"
+
+#define MAX_LEVEL 5
+
 struct RObj
 {
-
+    
 };
 
 struct SkiplistNode
 {
-    //robj* obj;
+    unsigned int value;
     double score;
     struct SkiplistNode* next;
     struct SkiplistLevel {
@@ -16,6 +20,16 @@ struct SkiplistNode
         unsigned int span;
     } level[];
 };
+
+int GetRandomLevel()
+{
+    int lev = 1;
+    while((RangeRandom(1, 10000) & 1) == 0)
+    {
+        ++lev;
+    }
+    return lev > MAX_LEVEL ? MAX_LEVEL : lev;
+}
 
 class Skiplist
 {
@@ -27,9 +41,9 @@ public:
     ~Skiplist();
 
 public:
-    SkiplistNode* insert();
-    SkiplistNode* find();
-    SkiplistNode* erase();
+    SkiplistNode* insert(unsigned int a_value, double a_score);
+    SkiplistNode* find(unsigned int a_value);
+    SkiplistNode* erase(unsigned int a_value);
 
     void dump();
 };
